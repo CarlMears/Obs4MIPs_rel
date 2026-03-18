ad9efacec4f665530c2fd5e1bd3e3db7a964236d
Carl Mears <mears@remss.com>
2026-03-18 11:43:23 -0700

    renamed the simple verions of the tb calc

--- Obs4MIPs_AMSU_forward_operator/rss_amsu_forward_operator/__init__.py
+++
@@ -1,6 +0,0 @@
-"""RSS AMSU forward operator package."""
-
-from .AMSU_forward_operator_table import AMSUForwardOperatorTable
-from .check_model_data import check_model_data, min_max_dict
-
-__all__ = ["AMSUForwardOperatorTable", "check_model_data", "min_max_dict"]
